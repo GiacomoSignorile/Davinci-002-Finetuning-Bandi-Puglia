e0e8f51e9daec6e70208e69f5c3e9aeef7fcf26a
Giacomo Signorile <79905812+GiacomoSignorile@users.noreply.github.com>
2024-02-12 20:11:47 +0100

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -110,7 +110,7 @@ if user_input := st.chat_input("Inserisci la tua domanda:"):
     with st.chat_message("user"):
         st.markdown(user_input)
 
-    result = chatbt_instance.qa({"question": user_input, "chat_history": chatbt_instance.chat_history["answer"]})
+    result = chatbt_instance.qa({"question": user_input, "chat_history": chatbt_instance.chat_history})
     chatbt_instance.chat_history.append([(user_input, result["answer"])])
     chatbt_instance.qa = chatbt_instance.load_db("stuff", 4)
     chatbt_instance.vector_store = chatbt_instance.load_vector_store()
